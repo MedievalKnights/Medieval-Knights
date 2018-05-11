ede070bae529110b17d0213bd87324cafa502b0d
RyanJabro <league@WTS-iMac-11.attlocal.net>
2018-05-10 17:39:11 -0700

    stuff

--- core/src/com/mygdx/game/Region.java
+++ core/src/com/mygdx/game/Region.java
@@ -11,18 +11,18 @@ public class Region {
 	TextureAtlas atlasBack = new TextureAtlas(Gdx.files.internal("Grass.atlas"));
 	Animation<TextureRegion> background = new Animation<TextureRegion>(1f,atlasBack.getRegions());
 	
-	Tile[][] region = new Tile[10][10];
+	Tile[][] region = new Tile[16][16];
 	public Region() {
-		for(int i = 0; i < 10;i++) {
-			for(int j = 0; j < 10;j++) {
-				region[i][j] = new Tile(i*32,i*32,background.getKeyFrame(1f));
+		for(int i = 0; i < 16;i++) {
+			for(int j = 0; j < 16;j++) {
+				region[i][j] = new Tile(i*32,j*32,background.getKeyFrame(1f));
 			}
 		}
 	}
 	
 	public void render(SpriteBatch batch) {
-		for(int i = 0; i < 10;i++) {
-			for(int j = 0; j < 10;j++) {
+		for(int i = 0; i < 16;i++) {
+			for(int j = 0; j < 16;j++) {
 				Tile t = region[i][j];
 				batch.draw(t.getTile(), t.getX(), t.getY(), 32, 32);
 			}
