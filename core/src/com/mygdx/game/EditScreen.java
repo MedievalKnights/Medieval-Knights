5433be3d9e3db833182c9ea9e54344e33b274c70
Jack <jpstevenson.avi@gmail.com>
2018-05-24 17:34:45 -0700

    Added EditScreen

--- core/src/com/mygdx/game/EditScreen.java
+++ core/src/com/mygdx/game/EditScreen.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameScreen {
+public class EditScreen {
 
 	World world;
 	MedievalKnights game; 
