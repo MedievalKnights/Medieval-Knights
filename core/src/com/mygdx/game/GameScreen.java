613cafa83bc8d04f3be8b341ef1c7e2f4a5180e9
Andrew-Seo <andrew.awesomeness5500@gmail.com>
2018-06-10 15:04:53 -0700

    desktop

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -17,16 +17,14 @@ public class GameScreen {
 		world = new World();
 		this.game = game;
 		inputs = new KeyInput();
-<<<<<<< HEAD
-		background = new Texture("Grass.png");
-		// background = new Texture("Grass_Complete_RPG.png");
+		//background = new Texture("Grass.png");
+		background = new Texture("Grass_Complete_RPG.png");
 		player = new Player(5, 2, "John", "A Guy", new Texture("New_Piskel.png"), 150, 150, world.getWorldX(),
 				world.getWorldY());
 		// player = new Player(100, 100, 100, 100, 5, 2, "John", "A Guy", new
 		// Texture("New_Piskel.png"), 150, 150, world.getWorldX(), world.getWorldY());
-=======
-		player = new Player(100,100,5,2, "John", "A Guy",new Texture("sprites/New_Piskel.png"),150,150,world.getWorldX(),world.getWorldY());
->>>>>>> MedievalKnights/master
+
+		player = new Player(100,100,"John", "A Guy",new Texture("sprites/New_Piskel.png"),150,150,world.getWorldX(),world.getWorldY());
 		inputs.setPlayer(player);
 		Gdx.input.setInputProcessor(inputs);
 
